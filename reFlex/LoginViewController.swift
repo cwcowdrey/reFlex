4db9c0c50ccd3a476b8b277a78614acbf935676d
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-02-28 11:08:01 -0500

    formatted login button

--- reFlex/LoginViewController.swift
+++ reFlex/LoginViewController.swift
@@ -8,14 +8,50 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    var canLogin = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupScreen()
     }
-
+    
+    
+    @IBAction func usernameChanged(_ sender: UITextField) {
+        checkForValidLogin()
+    }
+    
+    @IBAction func passwordChanged(_ sender: UITextField) {
+        checkForValidLogin()
+    }
+    
     @IBAction func loginButtonPressed(_ sender: UIButton) {
-        performSegue(withIdentifier: "loginSegue", sender: self)
+        
+        if canLogin {
+            performSegue(withIdentifier: "loginSegue", sender: self)
+        }
+    }
+    
+    
+    func setupScreen() {
+        loginButton.layer.cornerRadius = 10
+        loginButton.clipsToBounds = true
+    }
+    
+    func checkForValidLogin() {
+        if usernameTextField.text!.count > 6 && passwordTextField.text!.count > 6 {
+            loginButton.backgroundColor = .blue
+            canLogin = true
+        } else {
+            loginButton.backgroundColor = .red
+            canLogin = false
+        }
     }
     
 }
