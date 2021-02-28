770f2cd584045dc45899ff6f6dd6a207e9c8ee84
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-02-28 13:24:06 -0500

    add collection view cell

--- reFlex/Workout/Controller/WorkoutViewController.swift
+++ reFlex/Workout/Controller/WorkoutViewController.swift
@@ -8,10 +8,31 @@
 import Foundation
 import UIKit
 
-class WorkoutViewController: UIViewController {
+class WorkoutViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         print("got here")
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+        
+        cell.layer.borderWidth = 2
+        cell.layer.borderColor = .init(red: 200/256, green: 200/256, blue: 200/256, alpha: 1)
+                
+        return cell
     }
     
 }
---
+++ reFlex/Workout/Views/WorkoutCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  WorkoutCollectionViewCell.swift
+//  reFlex
+//
+//  Created by Christopher Cowdrey on 2/28/21.
+//
+
+import Foundation
+import UIKit
+
+class WorkoutCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textOk: UILabel!
+}
