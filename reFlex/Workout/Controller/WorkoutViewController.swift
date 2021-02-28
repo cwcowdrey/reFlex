cf6c083f9416d986f6ad77620036fa86ac0c2525
Christopher Cowdrey <cwcowdrey@gmail.com>
2021-02-28 12:55:14 -0500

    created vc

---
+++ reFlex/Workout/Controller/WorkoutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutViewController.swift
+//  reFlex
+//
+//  Created by Christopher Cowdrey on 2/28/21.
+//
+
+import Foundation
+import UIKit
+
+class WorkoutViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        print("got here")
+    }
+    
+}
